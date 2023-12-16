134b14b261423789f06afa10b8428d0828a6d031
Mathias <mathias.halmetschlager@gmx.at>
2023-12-16 13:41:33 +0100

    trpc setup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -15,6 +15,7 @@ import {
   AuthCredentialsValidator,
   TAuthCredentialsValidator,
 } from "@/lib/validators/account-credentials-validator";
+import { trpc } from "@/trpc/client";
 
 const Page = () => {
   const {
